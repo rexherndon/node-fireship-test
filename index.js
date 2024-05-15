a5a41a1303188ea7c0af7508660931577c7485dd
Rex Herndon <herndonrex1@gmail.com>
2024-05-15 10:26:51 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const { readFile } = require('fs').promises;
+const app = express();
+
+app.get('/', async (request, response) => {
+
+        response.send( await readFile('./home.html', 'utf8') );
+
+});
+
+
+app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`))
